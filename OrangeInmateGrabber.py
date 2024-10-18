4913e829b404c58e36ca23de1fea02ee7e5ebbd1
krongi <mgseverson83@gmail.com>
2024-10-18 09:57:05 -0400

    Fixed issue

--- OrangeInmateGrabber.py
+++ OrangeInmateGrabber.py
@@ -55,19 +55,18 @@ def get_inmate_details(json_file_string = 'general_inmate_info.json'):
 #     for entry in json_vars:
 #         image_list.append(entry.get('IMAGE'))
 
-
+def page_generator():
+    long_string = HTML_BEGIN + '\n'
+    with open('detail_inmate_info.json', 'r') as json_file:
+        json_vars = json.load(json_file)
+        for entry in json_vars:
+            head_string = f'<h1>{entry[0].get('NAME')}</h1>'
+            for_string = f'<img src="{entry[0].get('IMAGE')}">'
+            long_string = long_string + head_string + '/n' + for_string + '\n'
+    long_string = long_string + HTML_END
+    with open('long_html.html', 'w') as html_file:
+        html_file.write(long_string)
 
 # gather_json_list()
 # get_inmate_details('general_inmate_info.json')
-# get_inmate_images('detail_inmate_info.json')
-long_string = HTML_BEGIN + '\n'
-with open('SCSOGrabInmates/detail_inmate_info.json', 'r') as json_file:
-    json_vars = json.load(json_file)
-    for entry in json_vars:
-        for_string = f'<img src="{entry[0].get('IMAGE')}">'
-        long_string = long_string + for_string + '\n'
-long_string = long_string + HTML_END
-with open('long_html.html', 'w') as html_file:
-    html_file.write(long_string)
-# json_list = json_list[0]
-# get_inmate_details()
+# page_generator('detail_inmate_info.json')
